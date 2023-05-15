0651e0a34f1fa193be06813ab1f7cf80cd32bc2c
SerhiiChynin <sergey.chinin@gmail.com>
2023-05-15 16:51:59 +0300

    redux ts

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import { useTypesSelectors } from '../hooks/useTypesSelectors';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from '../store/action-creators/user';
+import type {} from 'redux-thunk/extend-redux';
 
 
 
 const UserList: React.FC = () => {
     const {users, error, loading} = useTypesSelectors(state => state.user);
+    const dispatch = useDispatch();
     // console.log(state);
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+        if (loading) {
+            return <h1>Идет загрузка...</h1>
+        }
+        if (error) {
+            return <h1>{error}</h1>
+        }
+    
     
     return (
         <div>
-
+            {users.map(user => 
+                <div key={user.id}>{user.name}</div>
+            )}
         </div>
     )
 }
--- src/store/action-creators/user.ts
+++ src/store/action-creators/user.ts
@@ -1,15 +1,23 @@
-import { UserActionTypes } from "../../types/todo"
+import axios from "axios";
+import { UserAction, UserActionTypes } from "../../types/todo"
 import { Dispatch } from 'redux';
-
+import UserList from "../../components/UserList";
 
 export const fetchUsers = () => {
-    return async (dispatch: Dispatch) => {
+    return async (dispatch: Dispatch<UserAction>) => {
         try {
-            dispatch({type: UserActionTypes.FETCH_USERS})
+            dispatch({type: UserActionTypes.FETCH_USERS })
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+            setTimeout(() => {
+                dispatch({type: UserActionTypes.FETCH_USERS_SUCCESS, payload: response.data})
+            }, 700)
         }
         catch (err) {
-            dispatch({type: UserActionTypes.FETCH_USERS_ERROR})
-            // https://jsonplaceholder.typicode.com/users
+            dispatch({
+                type: UserActionTypes.FETCH_USERS_ERROR,
+                payload: 'Ошибка при загрузке пользователей!'
+            })
+           
         }
     }
 }
