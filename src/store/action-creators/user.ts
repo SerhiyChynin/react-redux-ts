1fe229a06d1660e6ee2cfd65f31b6b6050cdba6a
SerhiiChynin <sergey.chinin@gmail.com>
2023-05-17 20:48:23 +0300

    todo list ts done

--- src/store/action-creators/user.ts
+++ src/store/action-creators/user.ts
@@ -10,7 +10,7 @@ export const fetchUsers = () => {
             const response = await axios.get('https://jsonplaceholder.typicode.com/users')
             setTimeout(() => {
                 dispatch({type: UserActionTypes.FETCH_USERS_SUCCESS, payload: response.data})
-            }, 700)
+            }, 500)
         }
         catch (err) {
             dispatch({
